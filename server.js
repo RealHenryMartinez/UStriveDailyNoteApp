c0d44cd78547717ff82c1a93cd73dc6990c1b30b
RealHenryMartinez <henrymtz0390@gmail.com>
2022-10-06 21:21:49 -0700

    heroku

--- server.js
+++ server.js
@@ -30,12 +30,16 @@ mongoose
 .then(() => console.log('Mongodb Connected...'))
 .catch((err) => console.error(err));
 
-if (process.env.MONGO_URI === "production"){
-    app.use(express.static("frontend/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-    })
-}
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('frontend/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 4020;
 
